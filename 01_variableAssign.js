fd05d7bb8963fbc3c8ae89e497403acf5b53335a
mabhishek042 <mabhishek0425@gmail.com>
2023-11-08 11:01:23 +0530

    Lecture_1 Assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,23 @@
+var myFullName;
+myFullName = "Abhishek More"
+console.log(myFullName);
+myFullName = "Abhishek Ravindra More"
+console.log(myFullName);
+console.log("---------------------------Step 2----------------------------------------------");
+var myLove = "I Love only JS";
+console.log(myLove);
+console.log("---------------------------Step 3----------------------------------------------");
+var firstName = "Abhishek";
+console.log(firstName);
+var lastName = "More";
+console.log(lastName);
+var age = 27;
+console.log(age);
+var address = "Karvenagar";
+console.log(address);
+var pincode = 411052;
+console.log(pincode);
+var gender = "Male";
+console.log(gender);
+var isMarried = false;
+console.log(isMarried);
---
+++ 02_typeOf.js
@@ -0,0 +1,22 @@
+var bankName;
+console.log(typeof bankName);
+bankName = "SBI Bank"
+console.log(bankName);
+console.log(typeof bankName);
+console.log("--------------------------Step 2-------------------------------------------");
+var marks = "90%"
+console.log(marks);
+console.log(typeof marks);
+console.log("--------------------------Step 3-------------------------------------------");
+var isWorking;
+console.log(typeof isWorking);
+isWorking = true;
+console.log(isWorking);
+console.log(typeof isWorking);
+console.log("--------------------------Step 4-------------------------------------------");
+var totalCount = "Hundred and Seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
